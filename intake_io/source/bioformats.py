dfa5cecde7105a1f2cf3b37c11d37047d2a94834
Burkhard Hoeckendorf <burkhard.hoeckendorf@pm.me>
2021-06-08 18:17:14 -0500

    Update BioformatsSource

--- intake_io/source/bioformats.py
+++ intake_io/source/bioformats.py
@@ -1,12 +1,54 @@
 import numpy as np
 import bioformats
 import xmltodict
-import intake
-from ..util import clean_yaml
-from typing import Optional
+from .base import ImageSource, Schema
 
 
-class BioformatsSource(intake.source.base.DataSource):
+def _parse_ome_metadata(xml: str):
+    xml = xmltodict.parse(xml)
+    pixmeta = xml["OME"]["Image"]["Pixels"]
+
+    axes = pixmeta["@DimensionOrder"][::-1].lower()
+    shape = {ax: int(pixmeta[f"@Size{ax.upper()}"]) for ax in axes}
+
+    axes = "".join(ax for ax in axes if shape[ax] > 1)
+    for ax in list(shape.keys()):
+        if ax not in axes:
+            shape.pop(ax)
+
+    spacing = {}
+    for ax in axes:
+        try:
+            spacing[ax] = float(pixmeta[f"@PhysicalSize{ax.upper()}"])
+        except KeyError:
+            pass
+
+    spacing_units = {}
+    for ax in axes:
+        try:
+            spacing_units[ax] = pixmeta[f"@PhysicalSize{ax.upper()}Unit"]
+        except KeyError:
+            pass
+
+    # parse channel names
+    try:
+        channels = tuple(i["@Name"] for i in pixmeta["Channel"])
+        coords = dict(c=channels)
+    except (KeyError, TypeError):
+        coords = {}
+
+    return {
+        "dtype": np.dtype(xml["OME"]["Image"]["Pixels"]["@Type"]),
+        "axes": axes,
+        "shape": shape,
+        "spacing": spacing,
+        "spacing_units": spacing_units,
+        "coords": coords,
+        "fileheader": xml
+    }
+
+
+class BioformatsSource(ImageSource):
     """Intake source using Bioformats as backend.
 
     Attributes:
@@ -18,75 +60,35 @@ class BioformatsSource(intake.source.base.DataSource):
     version = "0.0.1"
     partition_access = True
 
-    @staticmethod
-    def _static_get_schema(xml: str) -> intake.source.base.Schema:
-        h = xmltodict.parse(xml)
-        pixmeta = h["OME"]["Image"]["Pixels"]
-
-        # check for unknown axes
-        for d in pixmeta["@DimensionOrder"]:
-            assert d in "TCZYX"
-
-        # parse axes, shape and spacing
-        axes = ""
-        shape = []
-        spacing = []
-        for d in "TCZYX":
-            size = int(pixmeta[f"@Size{d}"])
-            if size > 1:
-                axes += d.lower()
-                shape.append(size)
-            if d in "ZYX":
-                try:
-                    spacing.append(float(pixmeta[f"@PhysicalSize{d}"]))
-                except KeyError:
-                    pass
-        shape = tuple(shape)
-        spacing = tuple(spacing)
-
-        # parse channel names
-        try:
-            channels = tuple(i["@Name"] for i in pixmeta["Channel"])
-            assert len(channels) == 1 and "c" not in axes or len(channels) == shape[axes.find("c")]
-            coords = dict(c=channels)
-        except (KeyError, TypeError):
-            coords = None
-
-        return intake.source.base.Schema(
-            datashape=shape,
-            shape=shape,
-            dtype=np.dtype(h["OME"]["Image"]["Pixels"]["@Type"]),
-            npartitions=shape[0],
-            chunks=None,
-            extra_metadata=dict(
-                axes=axes,
-                spacing=spacing,
-                spacing_units=None,
-                coords=coords,
-                fileheader=h
-            )
-        )
-
-    def __init__(self, uri: str, metadata: Optional[dict] = None):
+    def __init__(self, uri: str, **kwargs):
         """
         Arguments:
             uri (str): URI (e.g. file system path or URL)
             metadata (dict, optional): Extra metadata, handed over to intake
         """
-        super().__init__(metadata=metadata)
+        super().__init__(**kwargs)
         self.uri = uri
 
-    def _get_schema(self) -> intake.source.base.Schema:
+    def _get_schema(self) -> Schema:
         # Parse metadata, start a JVM if needed.
         # This JVM must continue to run until javabridge is no longer needed,
         # since javabridge does't support starting another one. Current
         # solution is to start a JVM upon first use of bioformats, and never
         # stop it.
         try:
-            return BioformatsSource._static_get_schema(bioformats.get_omexml_metadata(self.uri))
+            xml = bioformats.get_omexml_metadata(self.uri)
         except AttributeError:
             bioformats.javabridge.start_vm(class_path=bioformats.JARS, run_headless=True)
-            return BioformatsSource._static_get_schema(bioformats.get_omexml_metadata(self.uri))
+            xml = bioformats.get_omexml_metadata(self.uri)
+        xml = _parse_ome_metadata(xml)
+        shape = self._set_shape_metadata(xml["axes"], xml["shape"], xml["spacing"], xml["spacing_units"], xml["coords"])
+        self._set_fileheader(xml["fileheader"])
+        return Schema(
+            dtype=xml["dtype"],
+            shape=shape,
+            npartitions=1,
+            chunks=None
+        )
 
     def _get_partition(self, i: int) -> np.ndarray:
         all_axes = "tczyx"
@@ -106,27 +108,28 @@ class BioformatsSource(intake.source.base.DataSource):
 
     def read(self) -> np.ndarray:
         self._load_metadata()
-        axes = self.metadata["axes"]
-        shape = [self.shape[axes.find(d)] if d in axes else 1 for d in "tczyx"]
-        out = np.zeros(shape, self.dtype)
-        with bioformats.ImageReader(self.uri) as reader:
-            for t in range(out.shape[0]):
-                for c in range(out.shape[1]):
-                    for z in range(out.shape[2]):
-                        out[t, c, z] = reader.read(c=c, z=z, t=t, rescale=False)
-        return out.squeeze()
+        out = np.zeros(self.metadata["original_shape"], self.dtype)
+        if out.ndim == 2:
+            pass
+        else:
+            with bioformats.ImageReader(self.uri) as reader:
+                # it = np.nditer(out,
+                #    flags=("c_index", "multi_index"),
+                #    op_flags=("writeonly",),
+                #    op_axes=(list(range(out.ndim-2)),))
+                # while not it.finished:
+                #    ix = dict(zip(self.metadata["axes"][:len(it.multi_index)], it.multi_index))
+                #    out[it.multi_index] = reader.read(ix.get("c"), ix.get("z") or 0, ix.get("t") or 0, rescale=False)
+                #    it.iternext()
+
+                with np.nditer(out, flags=("multi_index",), op_flags=("readonly",),
+                               op_axes=(list(range(out.ndim - 2)),)) as ndit:
+                    for it in ndit:
+                        ix = dict(zip(self.metadata["original_axes"][:len(ndit.multi_index)], ndit.multi_index))
+                        # it[...] = reader.read(ix.get("c"), ix.get("z") or 0, ix.get("t") or 0, rescale=False)
+                        out[ndit.multi_index] = reader.read(ix.get("c"), ix.get("z") or 0, ix.get("t") or 0,
+                                                            rescale=False)
+        return self._reorder_axes(out)
 
     def _close(self):
         pass
-
-    def yaml(self, with_plugin=False, rename=None) -> str:
-        from yaml import dump
-        data = self._yaml(with_plugin)
-        data = clean_yaml(data, rename)
-        name = list(data["sources"].keys())[0]
-        for i in ("coords", "spacing"):
-            try:
-                data["sources"][name]["metadata"].pop(i)
-            except KeyError:
-                pass
-        return dump(data, default_flow_style=False)
