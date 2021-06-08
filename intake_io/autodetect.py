f8c248d4b2cc4f4b457d730fec4509962bc44a09
Burkhard Hoeckendorf <burkhard.hoeckendorf@pm.me>
2021-06-08 18:08:12 -0500

    Expose kwargs to imload

--- intake_io/autodetect.py
+++ intake_io/autodetect.py
@@ -5,9 +5,11 @@ import intake
 from . import source
 
 
-def autodetect(uri: str) -> intake.source.DataSource:
+def autodetect(uri: str, **kwargs) -> intake.source.DataSource:
     """
     Autodetect intake source given URI.
+
+    Keyword arguments are passed to the source constructor.
 
     If no other source is more suitable, it returns an instance of :class:`intake_io.source.ImageIOSource`, which uses
     `imageio <https://github.com/imageio/imageio>`_.
@@ -16,23 +18,27 @@ def autodetect(uri: str) -> intake.source.DataSource:
 
     :param uri:
         URI (e.g. file system path or URL)
+    :param kwargs:
+        Arguments passed to the source constructor
     :return:
         Data source
     """
     luri = uri.lower()
     lext = os.path.splitext(luri)[-1]
     if lext == ".nrrd":
-        return source.NrrdSource(uri)
+        return source.NrrdSource(uri, **kwargs)
+    elif lext in (".tif", ".tiff"):
+        return source.TifSource(uri, **kwargs)
     elif luri.endswith(".nii.gz") or lext == ".nii":
-        return source.NiftiSource(uri)
+        return source.NiftiSource(uri, **kwargs)
     elif lext in (".dicom", ".dcm"):
-        return source.DicomSource(uri)
+        return source.DicomSource(uri, **kwargs)
     elif luri.endswith(".dicom.zip") or luri.endswith(".dcm.zip"):
-        return source.DicomZipSource(uri)
+        return source.DicomZipSource(uri, **kwargs)
     elif lext == ".klb":
-        return source.KlbSource(uri)
-    elif luri.endswith(".ome.tif") or luri.endswith("ome.tiff") \
+        return source.KlbSource(uri, **kwargs)
+    elif luri.endswith(".ome.tif") or luri.endswith(".ome.tiff") \
             or lext not in (".tif", ".tiff", ".png", ".jpg", ".gif", ".mp4"):
-        return source.BioformatsSource(uri)
+        return source.BioformatsSource(uri, **kwargs)
     else:
-        return source.ImageIOSource(uri)
+        return source.ImageIOSource(uri, **kwargs)
--- intake_io/io.py
+++ intake_io/io.py
@@ -6,20 +6,52 @@ import zarr
 from typing import Any, Optional
 from .util import *
 from .autodetect import *
-from .source.imageio import save_tif
+from .source.tif import save_tif
 from .source.klb import save_klb
 from .source.nifti import save_nifti
 from .source.nrrd import save_nrrd
 
 
-def imload(uri: str, partition: Any = None, metadata_only: bool = False) -> xr.Dataset:
+def imload(uri: str, partition: Any = None, metadata_only: bool = False, **kwargs) -> xr.Dataset:
+    """
+    Load image, autodetect source type.
+
+    :param uri:
+    :param partition:
+        Load only a partition of the image (TODO: Support list of partitions)
+    :param metadata_only:
+        Load metadata only
+    :param kwargs:
+        Additional arguments passed to the source constructor. Notable fields supported by all subclasses of
+        ImageSource are:
+
+        output_axis_order (Optional[str]): deviate from the default "itczyx" output axis ordering, or None to use
+        original axis ordering of the data.
+
+        metadata (Optional[Dict[str, Any]]): add or overrule metadata fields, for instance:
+            metadata={
+                "axes": "tcxy",                       # specify input axis order
+                "spacing": {"t": 0.3},                # overrule spacing
+                "spacing_units": {"t": "s"},          # overrule spacing unit
+                "coords": {"c": ["nuclei", "actin"]}  # use specific channel names
+            }
+
+        TODO: Document catalog mechanism to manage external metadata and refer to it here.
+    :return:
+    """
     if isinstance(uri, intake.DataSource):
         if metadata_only:
             return uri.discover()
-        return xr.Dataset({"image": to_xarray(uri, partition=partition)})
+        out = xr.Dataset({"image": to_xarray(uri, partition=partition)})
+        out.attrs["metadata"] = {}
+        try:
+            out.attrs["metadata"]["spacing_units"] = out["image"].attrs["metadata"]["spacing_units"]
+        except KeyError:
+            pass
+        return out
     elif isinstance(uri, intake.catalog.entry.CatalogEntry):
         return imload(uri.get(), partition, metadata_only)
-    with autodetect(uri) as src:
+    with autodetect(uri, **kwargs) as src:
         return imload(src, partition, metadata_only)
 
 
